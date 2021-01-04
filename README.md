bcaaa8a703f15c142db7bf3dfbb2662302296175
Amaljith K <amaljith.kuttamath@radianarc.com>
2021-01-04 23:07:10 +0530

    ETM - manager

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Average time to execute these 2 commands is 100ms
 
-How to use :-
+Usage:-
 
+# Execution Time
 ```python
 from et.et import ET
 
@@ -13,8 +14,28 @@ execution_time = ET("info")
 """
 execution_time.stop()
 ```
-
-```output
+### Output:
+```
  info  | The execution time = 0:00:00.000010ms  | 15000 NS
  ```
+
+# ETM [Execution Time Manager]
+```python
+from et.et import ETM
+
+with ETM() as etm:
+    """
+    YOUR CODE HERE
+    """
+```
+### Output:
+```
+ | Started at | 2021-01-04 22:58:56.947392 |
+
+ | End at | 2021-01-04 22:58:56.947535  |
+
+  | Elapsed time | 0:00:00.000143 Sec | 
+```
+
+
 
--- et/et.py
+++ et/et.py
@@ -18,6 +18,56 @@ class ET():
         __dtx = self.__stop2 - self.__start2
         __dtz = self.__stop1 - self.__start1
         return print(f" {self.__info}  | The execution time = {__dtx}S  | {__dtz} NS" )
+    
+class ETM(object):
+    """ ETM used with the statement 'with' to time some execution.
+    Example:
+
+    with ETM() as etm:
+       # Code to time
+    """
+
+    def __enter__(self):
+        """
+        """
+        self.start = dt.now()
+        self.log('\n | Started at | {} |')
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        """
+        self.endlog()
+
+        return False
+
+    def log(self, s, elapsed=None):
+        """Log current time and elapsed time if present.
+        :param s: Text to display, use '{}' to format the text with
+            the current time.
+        :param elapsed: Elapsed time to display. Dafault: None, no display.
+        """
+        # print(s.format(self._secondsToStr(self.clock())))
+        print(s.format(dt.now()))
+
+        if(elapsed is not None):
+            print('\n  | Elapsed time | {} Sec | \n'.format(elapsed))
+
+    def endlog(self):
+        """Log time for the end of execution with elapsed time.
+        """
+        self.log(' | End at | {}  |', self.now())
+
+    def now(self):
+        """Return current elapsed time as hh:mm:ss string.
+        :return: String.
+        """
+        self.seconds = dt.now() - self.start
+        return self.seconds
 
 # execution_time = ET("info")
 # ET("self").stop()
+
+# with ETM() as etm:
+#     print("xyz")
